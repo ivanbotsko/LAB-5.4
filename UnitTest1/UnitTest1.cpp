bbd5c7319ecf7ed5e968db91e15db8167fc2ba6f
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-10-24 18:18:09 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR 5.4/PR 5.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestS0)
+		{
+			
+			double result = S0(1); 
+			double expected = 2; 
+			double epsilon = 0.0001; 
+
+			
+			Assert::AreEqual(result, expected, epsilon);
+		}
+	};
+}
